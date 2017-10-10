6db3db465797d91412fb16391e800a963804024b
Cameron Eldridge <cameldridge+git@gmail.com>
2017-10-10 13:15:44 -0400

    Update configs to get everything up to date

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,31 @@
 'use strict';
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = [
   {
     entry: './src/client/index.js',
     output: {
-      path: './public_html',
+      path: path.resolve('./public_html'),
       filename: 'machi-koro.js'
     },
     module: {
       rules: [
         { test: /\.jsx?$/, exclude: /node_modules.*\.js/, loader: 'babel-loader' },
-        { test: /\.s(a|c)ss$/, exclude: /node_modules.*\.js/, loader: 'style-loader!css-loader!postcss-loader!sass-loader' },
+        { test: /\.s(a|c)ss$/, exclude: /node_modules.*\.js/, loader: 'style-loader!css-loader!postcss-loader!fast-sass-loader' },
         { test: /\.(gif|svg|png|jpe?g)$/, loader: 'file-loader?name=images/[hash].[ext]!img-loader' }
       ]
-    }
+    },
+    plugins: [
+      new UglifyJSPlugin(),
+      new webpack.EnvironmentPlugin(['NODE_ENV'])
+    ],
   },
   {
     entry: './src/server/index.js',
     output: {
-      path: './',
+      path: path.resolve('./'),
       filename: 'server.js'
     },
     target: 'node',
@@ -30,6 +38,10 @@ module.exports = [
       // NOTE: this looks kind of wrong. maybe there's a real way to do it.. hm?
       'express': `require('express')`,
       'socket.io': `require('socket.io')`
-    }
+    },
+    plugins: [
+      new UglifyJSPlugin(),
+      new webpack.EnvironmentPlugin(['NODE_ENV'])
+    ],
   }
 ]
