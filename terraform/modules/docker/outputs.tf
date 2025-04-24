fcec881291ae43d85e874a5b99fc2f9a2c1b2940
Cameron Eldridge <cameldridge@gmail.com>
2025-04-23 22:24:36 -0400

    update networking options

--- terraform/modules/docker/outputs.tf
+++ terraform/modules/docker/outputs.tf
@@ -1,3 +1,11 @@
+output "container_port" {
+  value = local.internal_port
+}
+
 output "port" {
-  value = docker_container.machi-koro.ports[0].external
+  value = var.expose ? docker_container.machi-koro.ports[0].external : null
+}
+
+output "name" {
+  value = docker_container.machi-koro.name
 }
